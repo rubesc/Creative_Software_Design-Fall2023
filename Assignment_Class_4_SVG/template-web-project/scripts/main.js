5c01875d41a796dd6e34d205e0fa0a7d42e9f7e2
rubesc <ruby.w.chu@gmail.com>
2023-09-24 23:25:57 -0400

    added css redo

---
+++ Assignment_Class_4_SVG/template-web-project/scripts/main.js
@@ -0,0 +1,8 @@
+newdiv = document.createElement('div');
+document.querySelector('body').appendChild(newdiv);
+console.log('blah blah blah')
+console.log('blah')
+console.log('blah blah blah')
+// console.log('HI IM WORKING');
+
+
