8df8b0098b1ee691ab2bd515a5a3b8a0776eb2ae
rubesc <ruby.w.chu@gmail.com>
2023-09-27 19:06:49 -0400

    updated javascript reference

---
+++ Assignment_Class_5_Javascript_intro/scripts/main.js
@@ -0,0 +1,31 @@
+
+
+// newdiv = document.createElement('div');
+// document.querySelector('body').appendChild(newdiv);
+// console.log('HI IM WORKING');
+
+// const obj = { name: 'omar' };
+
+
+// create a new element 
+let x = document.createElement('div') ; // create element
+x.classList.add('btn'); // add class to element
+x.classList.remove('btn')
+x.classList.add('testing'); 
+
+
+// x.innerHTML = '<p> the first row </p> <p> the second row </p>'
+// x.innerText = 'wow my first real javascript element'
+// x.style.color = 'red'
+// x.style['transform'] = 'skew(50deg)'
+
+
+div.innerText = 'dude whats up';
+document.querySelector('h1').innerText = 'this is now an official header'
+
+// Add new element to "container "
+document.querySelector('.container').appendChild(x);
+
+
+console.log(x)
+console.log(document.querySelector('.container'))
